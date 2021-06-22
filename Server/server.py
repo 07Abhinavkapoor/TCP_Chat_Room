7a7f1d3b493e7b720e9f7088c7eaee50fac0da8e
Abhinav <07abhinavkapoor@gmail.com>
2021-06-22 20:45:06 +0530

    Refactored server.py

--- Server/server.py
+++ Server/server.py
@@ -3,12 +3,15 @@ import socket
 
 class Server:
     def __init__(self):
+        self.initialise_server()
+        self.receive_and_accept()
+
+    def initialise_server(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind(("127.0.0.1", 48_531))
         print("Server Initialised ........")
         self.server.listen()
         print("Listening .......")
-        self.receive_and_accept()
 
     def receive_and_accept(self):
         while True:
