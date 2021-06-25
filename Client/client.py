5448fc4465625c16f80f1c6a5b3983c4f633edfc
Abhinav <07abhinavkapoor@gmail.com>
2021-06-25 20:52:07 +0530

    Added and Implemented green_signal on client side.
    Called receive() method in the main thread.
    Set send() method thread as the daemon thread.
    Added green_signal keyword and implemented its usage in the code.

--- Client/client.py
+++ Client/client.py
@@ -1,31 +1,57 @@
 import socket
 import threading
+import json
+from pathlib import Path
 
 
 class Client:
     def __init__(self):
+        self.keywords = json.loads(Path("keywords.json").read_text())
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(("127.0.0.1", 48_531))
         self.start()
 
+    def get_nickname(self):
+        print("Enter your nickname: ")
+        self.nickname = input("> ")
+
     def receive(self):
+
         while True:
             message = self.client.recv(1024).decode("ascii")
             if len(message) == 0:
                 self.client.close()
                 break
+            elif message == self.keywords["nickname"]:
+                self.set_up_profile()
+            else:
+                print(message)
+
+    def set_up_profile(self):
+        self.get_nickname()
+        self.client.send(self.nickname.encode("ascii"))
+        message = self.client.recv(1024).decode("ascii")
+        if len(message) == 0:
+            self.client.close()
+        elif message == self.keywords["green_signal"]:
+            self.event.set()
+        else:
             print(message)
+            self.set_up_profile()
 
     def send(self):
+        self.event.wait()
+
         while True:
-            message = input()
+            message = f"{self.nickname} - {input()}"
             self.client.send(message.encode("ascii"))
 
     def start(self):
-        receive_thread = threading.Thread(target=self.receive)
-        send_thread = threading.Thread(target=self.send)
-        receive_thread.start()
+        self.event = threading.Event()
+
+        send_thread = threading.Thread(target=self.send, daemon=True)
         send_thread.start()
+        self.receive()
 
 
 if __name__ == "__main__":
